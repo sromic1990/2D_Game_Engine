221b750174ce70320075a3c3513f1411f0a042a2
Sourav Chatterjee <sromic@gmail.com>
2017-11-23 18:22:52 +0530

    Initial Upload
    -Simple skeletal engine that can render a vertex shader and a fragment shader glsl on a webpage.

---
+++ GameEngine/public_html/src/GLSLShaders/SimpleFS.glsl
@@ -0,0 +1,7 @@
+
+precision mediump float;    // sets the precision for floating point computation
+uniform vec4 uPixelColor;   //to transform the vertex position
+void main(void)
+{
+    gl_FragColor = uPixelColor;
+}
---
+++ GameEngine/public_html/src/GLSLShaders/SimpleVS.glsl
@@ -0,0 +1,7 @@
+
+attribute vec3 aSquareVertexPosition;
+
+void main(void)
+{
+    gl_Position = vec4(aSquareVertexPosition, 1.0);
+}
